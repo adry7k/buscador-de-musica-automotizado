f6c19a98eed90b0412a1c3366e764b6af89353cb
adry7k <adriamusashi455@gmail.com>
2024-10-07 21:51:23 -0300

    comand2.py

---
+++ comand.py
@@ -0,0 +1,9 @@
+import pyautogui
+import time
+pyautogui.PAUSE = 0.5
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+pyautogui.write("https://youtu.be/9EKi2E9dVY8?si=1_Hf8K_EEqcYgGdd")
+pyautogui.press("space")
